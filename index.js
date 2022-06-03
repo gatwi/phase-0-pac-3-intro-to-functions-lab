9077fa9d79480ae44bb49f9824a1007cb1cefc8a
gatwirimurithi <faith.murithi@student.moringaschool.com>
2022-06-03 21:50:33 +0300

    commit to master

---
+++ index.js
@@ -0,0 +1,14 @@
+function shout(string) {
+    // todo
+    return string.toUpperCase();
+}
+"Hello!".toUpperCase(); // 'HELLO!'
+function whisper(string) {
+    // todo
+    return string.toLowerCase();
+}
+"HELLO!".toLowerCase(); // 'Hello!'
+function logShout(string) {
+    //todo
+    console.log()
+}
